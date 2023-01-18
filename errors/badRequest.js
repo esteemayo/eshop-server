65377c2f8ba52f6d2eef2896633621504bf78e6e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-18 15:33:06 +0100

    fix: convert from common.js to es6 module

--- errors/badRequest.js
+++ errors/badRequest.js
@@ -1,5 +1,5 @@
-const { StatusCodes } = require('http-status-codes');
-const CustomAPIError = require('./customApiError');
+import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from './customApiError.js';
 
 class BadRequestError extends CustomAPIError {
   constructor(message) {
@@ -10,4 +10,4 @@ class BadRequestError extends CustomAPIError {
   }
 }
 
-module.exports = BadRequestError;
+export default BadRequestError;
