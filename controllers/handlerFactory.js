2375b8e665ef6df6f909a056a9560a916fe060e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-18 15:15:54 +0100

    fix: convert from common.js to es6 module

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,8 +1,8 @@
-const { StatusCodes } = require('http-status-codes');
+import { StatusCodes } from 'http-status-codes';
 
-const APIFeatures = require('../utils/apiFeatures');
-const NotFoundError = require('../errors/notFound');
-const asyncWrapper = require('../utils/asyncWrapper');
+import APIFeatures from '../utils/apiFeatures.js';
+import asyncWrapper from '../utils/asyncWrapper.js';
+import NotFoundError from '../errors/notFound.js';
 
 exports.getAll = (Model) =>
   asyncWrapper(async (req, res, next) => {
