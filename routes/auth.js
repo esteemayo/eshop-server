2867762e8eb7e3e7995f4ba68db878d82d60cb7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-18 15:53:53 +0100

    fix: convert to es6 and refactor

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express';
 
-const authController = require('../controllers/authController');
+import * as authController from '../controllers/authController.js';
 
 const router = express.Router();
 
@@ -18,4 +18,4 @@ router.patch(
   authController.updatePassword
 );
 
-module.exports = router;
+export default router;
