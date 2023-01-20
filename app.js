4bc27ba8b0be0eabc1588701a72013866ff2d3ea
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-20 06:09:24 +0100

    fix: modify

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 
 // cookie parser middleware
-app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
+app.use(cookieParser());
 
 // data sanitization against NoSQL query injection
 app.use(mongoSanitize());
