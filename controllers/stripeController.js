f82f4eed0d1de0c1074369bb97b29045041de579
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-26 18:00:47 +0100

    fix: fix a bug

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,23 +1,18 @@
-import stripe from 'stripe';
+import Stripe from 'stripe';
 import { StatusCodes } from 'http-status-codes';
 
 import asyncWrapper from '../utils/asyncWrapper.js';
-import CustomAPIError from '../errors/customApiError.js';
 
-stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const payment = asyncWrapper(async (req, res, next) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: 'usd',
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        return next(new CustomAPIError(stripeErr));
-      }
-      return res.status(StatusCodes.OK).json(stripeRes);
-    }
-  );
+  const stripeRes = await stripe.charges.create({
+    source: req.body.tokenId,
+    amount: req.body.amount,
+    currency: 'usd',
+  }, {
+    apiKey: process.env.STRIPE_SECRET_KEY,
+  });
+
+  res.status(StatusCodes.OK).json(stripeRes);
 });
