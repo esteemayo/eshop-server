b0948482a1d560f318a33f8007251079d9b068c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-18 00:57:58 +0100

    fix: modify

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -8,7 +8,6 @@ const createSendToken = (user, statusCode, req, res) => {
     httpOnly: true,
     sameSite: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    path: '/',
   });
 
   const { password, role, ...rest } = user._doc;
